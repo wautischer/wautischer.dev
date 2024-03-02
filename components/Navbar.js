c07854454fbb9bc6242e8bef3c112807282845b2
Laurin Wautischer <78680534+wautischer@users.noreply.github.com>
2024-03-02 19:06:33 +0100

    beautifie navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,10 +2,13 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-sm bg-transparent navbar-dark">
             <div className="container-fluid">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#collapsibleNavbar">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                <span className="d-sm-none fs-3 fw-bold">L. Wautischer</span>
+                <div className="ms-auto">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#collapsibleNavbar">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                </div>
                 <div className="collapse navbar-collapse" id="collapsibleNavbar">
                     <ul className="navbar-nav">
                         <li className="nav-item">
@@ -21,7 +24,6 @@ const Navbar = () => {
                 </div>
             </div>
         </nav>
-
     );
 };
 
