df49a557b176a7d6c39d655859cb69b6c5410177
Laurin Wautischer <laurin@Laptop-von-Laurin.local>
2024-05-08 10:42:46 +0200

    update project

---
+++ pages/projects/app/rate-right-light.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import Navbar from '/components/Navbar';
+
+export default function rateRightLight() {
+    return (
+        <div>
+            <Head>
+                <title>Projects - Rate Right Light</title>
+                <link rel="icon" type="image/png" href="/logosmall.png" />
+            </Head>
+            <Navbar />
+            <div>
+                <h1 className="text-center text-danger">DETAILS COMING SOON!</h1>
+            </div>
+        </div>
+    );
+}
